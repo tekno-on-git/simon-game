cfc7b143eb127881d248ccb5214f12748862b5d5
codeph-0bia <codephobia642@gmail.com>
2023-10-03 00:16:32 +0530

    game logic implemented

--- script.js
+++ script.js
@@ -1,14 +1,68 @@
 btnColors = ["red", "blue", "green", "yellow"];
 
-let next = () => Math.floor(Math.random() * 4);
+let rand = () => Math.floor(Math.random() * 4);
 
 let seq = [];
+let userSeq = [];
 
-//TODO: Flashing animation on click and sounds
+let keypressed = false;
+
+//create seq
+function nextSeq() {
+    let randomChoosenColor = btnColors[rand()];
+    seq.push(randomChoosenColor);
+    $("#" + randomChoosenColor).fadeOut(100).fadeIn(100);
+    playSound(randomChoosenColor);
+    $("h1").text("Level " + seq.length)
+        //TODO: white flash
+}
+
+//key press for first seq
+$(document).on("keypress", function() {
+    if (!keypressed) {
+        keypressed = !keypressed;
+        nextSeq();
+    }
+})
+
 
 $(".btn").on("click", function(event) {
-    let id = event.target.id;
-    $("#" + id).fadeOut(10).fadeIn(10);
-    let audio = new Audio("sounds/" + id + ".mp3");
-    audio.play();
+
+
+    if (!keypressed) {
+        return;
+    }
+    let randColor = event.target.id;
+    userSeq.push(randColor);
+    $("#" + randColor).fadeOut(100).fadeIn(100);
+    playSound(randColor);
+    //logic to catch wrong :
+    // us[us.l - 1] != seq[us.l - 1] -> wrongState()
+    // us[us.l - 1] == seq[us.l - 1] -> right
+
+    if (userSeq[userSeq.length - 1] != seq[userSeq.length - 1]) {
+        wrongState();
+        return;
+    }
+
+    //us.l == seq.l -> nextSeq()
+    if (userSeq.length == seq.length) {
+        userSeq.length = 0;
+        setTimeout(nextSeq, 1000);
+        return;
+    }
 })
+
+function playSound(key) {
+    let audio = new Audio("sounds/" + key + ".mp3");
+    audio.play();
+}
+
+function wrongState() {
+    seq.length = 0;
+    userSeq.length = 0;
+    playSound("wrong");
+    keypressed = false;
+    $("h1").text("Game over, Press any key to Restart.")
+        //TODO: red flash
+}
